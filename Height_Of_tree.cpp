0a241da41ad83ab757cc74132b155e3f05ab749b
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2023-10-09 08:15:02 +0530

    height of binary tree

---
+++ Height_Of_tree.cpp
@@ -0,0 +1,28 @@
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        //Base case
+        if(!node){
+            return 0;
+        }
+        
+        int leftHeight = height(node->left);
+        int rightHeight = height(node->right);
+        
+        return 1+ max(leftHeight, rightHeight);
+    }
+};
